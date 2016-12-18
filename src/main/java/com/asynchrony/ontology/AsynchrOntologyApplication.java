b28ede709bc7c35c33ae35e888c156f161e77bb4
Mike Pleimann <pleimann@users.noreply.github.com>
2016-12-18 08:36:20 -0600

    Initial Commit

---
+++ src/main/java/com/asynchrony/ontology/AsynchrOntologyApplication.java
@@ -0,0 +1,12 @@
+package com.asynchrony.ontology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsynchrOntologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsynchrOntologyApplication.class, args);
+	}
+}
---
+++ src/main/java/com/asynchrony/ontology/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.asynchrony.ontology;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AsynchrOntologyApplication.class);
+	}
+
+}
